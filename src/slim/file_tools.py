1fc9624be3a45e6417e02af04d6fdaafdae7c9e4
Thomas Svensson <thomas@tsvenson.com>
2022-04-18 09:57:03 +0200

    Passes mypy --strict.

--- src/slim/file_tools.py
+++ src/slim/file_tools.py
@@ -1,8 +1,10 @@
 import os
 import json
 
+from typing import List, Any
 
-def list_projects(path: str) -> list:
+
+def list_projects(path: str) -> List[str]:
     file_list = []
 
     for entry in os.scandir(path):
@@ -12,7 +14,8 @@ def list_projects(path: str) -> list:
     return file_list
 
 
-def read_json_file(project_file: str) -> dict:
+def read_json_file(project_file: str) -> Any:
     with open(project_file) as f:
         project_data = json.load(f)
+        print(type(project_data))
     return project_data
